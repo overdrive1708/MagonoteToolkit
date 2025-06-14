48f57be280d9f9f6e5e66032ad2145ca16d6981e
overdrive1708 <52090022+overdrive1708@users.noreply.github.com>
2025-06-14 16:45:13 +0900

    Create exception handler.

--- src/MagonoteToolkit/App.xaml.cs
+++ src/MagonoteToolkit/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using MagonoteToolkit.Models;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace MagonoteToolkit
 {
@@ -7,6 +10,22 @@ namespace MagonoteToolkit
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Startup処理
+        /// </summary>
+        /// <param name="sender">イベントソース</param>
+        /// <param name="e">イベントデータ</param>
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // 未処理の例外が発生したときの処理を登録する｡
+            DispatcherUnhandledException += ExceptionHandler.OnDispatcherUnhandledException;
+            TaskScheduler.UnobservedTaskException += ExceptionHandler.OnUnobservedTaskException;
+            AppDomain.CurrentDomain.UnhandledException += ExceptionHandler.OnUnhandledException;
+
+            // MainWindowを表示する｡
+            MainWindow mainWindow = new();
+            mainWindow.Show();
+        }
     }
 
 }
